73924d60da06b3725d5dd0c796b7e472e6ab15c2
juandurefaccini <juanchidure@outlook.com>
2022-03-13 08:37:19 -0300

    Elimnar console log innecesario

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -5,8 +5,6 @@ import React from "react";
 export function ProtectedRoute({ children }) {
   const { user, loading } = useAuth();
 
-  console.log(user);
-
   if (loading) return <h1>loading...</h1>; // Si el usuario esta cargando retorna cargando
 
   if (!user) return <Navigate to="/login" />; // Si no esta logueado retorna al login
