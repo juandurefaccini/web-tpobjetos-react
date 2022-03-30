da1a9aaa44ffdb75403ee6d6a386baeb50589558
Juan Dure <juanchidure@outlook.com>
2022-03-30 18:38:05 -0300

    Estilar la busqueda de elementos

--- src/components/Searcher.jsx
+++ src/components/Searcher.jsx
@@ -2,20 +2,50 @@ import React, { useEffect } from "react";
 import { useFileExplorer } from "../context/fileExplorerContext";
 import { useServices } from "../context/servicesContext";
 import FileList from "./Explorer/FileList";
+import Alert from "./Alert";
+
+function SearcherContainer({ children }) {
+  return (
+    <div className="flex justify-center items-center  w-full">{children}</div>
+  );
+}
 
 export default function Searcher() {
   const { getArchivoFiltro } = useServices();
   const { search } = useFileExplorer();
   const [searchOutput, setSearchOutput] = React.useState([]);
+  const [loading, setLoading] = React.useState(true);
+  const [error, setError] = React.useState(false);
 
   useEffect(() => {
-    getArchivoFiltro(search, ":root").then((res) => {
-      setSearchOutput(res);
-    });
+    getArchivoFiltro(search, ":root")
+      .then((res) => {
+        setSearchOutput(res);
+        setLoading(false);
+      })
+      .catch((err) => {
+        setError(err.message);
+      });
   }, [search]);
 
-  if (searchOutput == undefined || searchOutput.length === 0)
-    return <>No hay resultados</>;
+  if (error) {
+    return (
+      <SearcherContainer>
+        <Alert message={error} />
+      </SearcherContainer>
+    );
+  }
 
-  return <FileList files={searchOutput} />;
+  if (loading) {
+    return <SearcherContainer>Cargando...</SearcherContainer>;
+  }
+
+  if (searchOutput == undefined || searchOutput.length === 0)
+    return <SearcherContainer> No hay resultados </SearcherContainer>;
+
+  return (
+    <SearcherContainer>
+      <FileList files={searchOutput} />
+    </SearcherContainer>
+  );
 }
